650678729e55afb134d57a3767d7b22757c040a6
Dan Jacob <dansamueljacob@gmail.com>
2017-08-01 20:24:26 -0400

    enable anon login

--- chatsnap/src/app/app.module.ts
+++ chatsnap/src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+
+
+export const firebaseConfig = {
+  apiKey:  "AIzaSyACYQ1vKZYfXRUbUdV-ivQtKSycBqFCO0U",
+  authDomain:  "chat-app-34d2f.firebaseapp.com",
+  databaseURL: "https://chat-app-34d2f.firebaseio.com",
+  storageBucket: "chat-app-34d2f.appspot.com"
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
